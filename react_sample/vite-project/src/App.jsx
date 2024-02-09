baa4fc91061fc85d2b1f89c5839527ab8ca98780
Anandakrishnan345 <ckanandu@gmail>
2024-02-09 15:57:37 +0530

    react4

--- react_sample/vite-project/src/App.jsx
+++ react_sample/vite-project/src/App.jsx
@@ -1,13 +1,15 @@
-import Greeting from "./component/Greeting";
-import Greeting1 from "./component/Greeting1";
-import Counter from "./component/counter";
+// import Greeting from "./component/Greeting";
+// import Greeting1 from "./component/Greeting1";
+// import Counter from "./component/counter";
+import Parent from "./component/forms/Parent"
 
 function App() {
   return (
   <>
-   <Greeting/>
+   {/* <Greeting/>
    <Greeting1 name = {"bunny"}/>
-  <Counter initialValue = {0}/>
+  <Counter initialValue = {0}/> */}
+  <Parent/>
   </>
   )
   
--- react_sample/vite-project/src/component/forms/Input.jsx
+++ react_sample/vite-project/src/component/forms/Input.jsx
@@ -10,4 +10,13 @@ function Input({onsubmit}){
     const handleInputChange = (e)=>{
         setName(e.target.value)
     }
+    return(
+        <>
+        <form onSubmit={onSubmit}>
+            <input type="text" name="name" id="name" placeholder="enter name" onChange={handleInputChange} />
+            <input type="submit" />
+        </form>
+        </>
+    )
 }
+export default Input;
